3e7be262ba85c9915d2e8bc1584cb74f7c0f1b13
RodrigoF <fernandes.rodrigo.p1@gmail.com>
2022-06-30 09:48:29 -0300

    css changes

--- digimonapi/src/App.js
+++ digimonapi/src/App.js
@@ -1,8 +1,17 @@
 import Characters from './pages/Characters';
-
+import digimonLogo from './assets/imgs/DigimonLogo.png'
+import style from './App.module.scss'
 const App = () => {
 
-  return <Characters/>;
+  return (
+    <main className={style.digimonMain}>
+      <img className={style.digimonLogo}src={digimonLogo} alt="Digimon Logo" />
+      <Characters/>;
+
+    </main>
+    
+  )
+  
 }
 
 export default App;
